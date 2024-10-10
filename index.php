5b6a7f6d7e915b39f4d8a96b5f6dbf81bf9c42ba
etheread <160173197+etheread@users.noreply.github.com>
2024-10-10 14:12:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>very intresting website</title>
+</head>
+<body>
+    <?php
+        for($bebe = 1;$bebe<= 10000000000000;$bebe++){
+            echo "NIGGERS <br>";
+            echo "NIGGERS";
+        }
+    ?>
+</body>
+</html>
